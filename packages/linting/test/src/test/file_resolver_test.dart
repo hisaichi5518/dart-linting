4443572b91cf4cbd24460bcca06ad88691fc5f57
hisaichi5518 <hisaichi5518@gmail.com>
2021-09-27 01:15:36 +0900

    Add FileResolver

---
+++ packages/linting/test/src/test/file_resolver_test.dart
@@ -0,0 +1,22 @@
+import 'package:linting/test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FileResolver', () {
+    test('has file', () async {
+      final path = "test/src/test/examples/test.dart";
+      final unit = await FileResolver.resolve(path);
+      expect(unit.exists, isTrue);
+    });
+
+    test('no file', () async {
+      final path = "test/src/test/examples/test_not_found.dart";
+      try {
+        await FileResolver.resolve(path);
+        fail("failed");
+      } catch (error) {
+        expect(error, isA<ArgumentError>());
+      }
+    });
+  });
+}
