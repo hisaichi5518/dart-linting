5404b35c7569048a535f1a383ed462a8da95d6ee
hisaichi5518 <hisaichi5518@gmail.com>
2021-10-09 20:29:14 +0900

    export files

--- packages/linting/lib/linting.dart
+++ packages/linting/lib/linting.dart
@@ -1,2 +1,10 @@
 library linting;
 
+export 'src/analysis_options_loader/analysis_options_loader.dart';
+export 'src/analyzer/linting_analyzer.dart';
+export 'src/analyzer/model/analyzed_result.dart';
+export 'src/analyzer/model/analyzer_config.dart';
+export 'src/analyzer/model/internal_resolved_unit_result.dart';
+export 'src/analyzer/model/issue.dart';
+export 'src/analyzer/model/replacement.dart';
+export 'src/analyzer/model/rule.dart';
