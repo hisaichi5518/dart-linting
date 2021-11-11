5324d3da5770e66ff3e31f280f6d32fc7abc64db
hisaichi5518 <hisaichi5518@gmail.com>
2021-11-12 00:34:45 +0900

    get next version from branch name

--- .github/workflows/scripts/get_current_version.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import sys
-import utils
-
-# python .github/workflows/scripts/get_current_version.py tools/analyzer_plugin/pubspec.yaml
-pubspec_yaml = sys.argv[1] if len(sys.argv) > 1 else 'pubspec.yaml'
-version = utils.get_version_from_pubspec(pubspec_yaml)
-print(version)
