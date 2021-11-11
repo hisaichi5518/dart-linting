132d0d485d823798530694a4e32097774ce0d43c
hisaichi5518 <hisaichi5518@gmail.com>
2021-11-11 22:44:15 +0900

    oops

--- .github/workflows/scripts/override_pubspec_version.py
+++ .github/workflows/scripts/override_pubspec_version.py
@@ -3,9 +3,6 @@ import sys
 import utils
 
 # python .github/workflows/scripts/override_version.py example/tools/analyzer_plugin/pubspec.yaml 1.1.0
-# pubspec_yaml = sys.argv[1]
-# version = sys.argv[2]
-# utils.override_version(pubspec_yaml, version)
-
-
-utils.override_changelog('CHANGELOG.md', '1.2.0', '- 変更内容')
+pubspec_yaml = sys.argv[1]
+version = sys.argv[2]
+utils.override_version(pubspec_yaml, version)
