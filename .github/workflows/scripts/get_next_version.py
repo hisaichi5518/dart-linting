ce253fda565cb8d6388873bf0549168398262fc1
hisaichi5518 <hisaichi5518@gmail.com>
2021-11-12 00:49:12 +0900

    Remove get_next_version

--- .github/workflows/scripts/get_next_version.py
+++ .github/workflows/scripts/get_next_version.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 import sys
-import utils
 
 # python .github/workflows/scripts/get_next_version.py release/1.1.0
 version = sys.argv[1] if len(sys.argv) > 1 else 'release/1.0.0'
 version = version.replace('release/', '')
-print(utils.get_next_version(version))
+print(version)
--- .github/workflows/scripts/utils.py
+++ .github/workflows/scripts/utils.py
@@ -9,19 +9,6 @@ def get_version_from_pubspec(pubspec_yaml):
             if result:
                 return result.group(1)
 
-
-def get_next_version(version):
-    def mapping(i_n):
-        if i_n[0] == 0:
-            return i_n[1]
-        elif i_n[0] == 1:
-            return str(int(i_n[1]) + 1)
-        else:
-            return '0'
-
-    vers = map(mapping, enumerate(version.split('.')))
-    return '.'.join(vers)
-
 def override_version(pubspec_yaml, version):
     with open(pubspec_yaml, 'r') as f:
         text = re.sub(r'refs/tags/[0-9.]+', 'refs/tags/%s' % version, f.read())
